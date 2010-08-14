e510ee73996a9380fd2b2cb2318a0ab2ab06c9a8
Marcio Rosa <protonss@ig.com.br>
2010-08-14 10:13:45 -0300

    IDENTANDO O CODIGO DO PROJETO ServletsComOutrosMets

--- ServletsComOutrosMets/src/meupacote/RecCabecalhos.java
+++ ServletsComOutrosMets/src/meupacote/RecCabecalhos.java
@@ -10,38 +10,47 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class for Servlet: RecCabecalhos
- *
+ * 
  */
- public class RecCabecalhos extends javax.servlet.http.HttpServlet {
-    /* (non-Java-doc)
+public class RecCabecalhos extends javax.servlet.http.HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7285075619890732192L;
+
+	/*
+	 * (non-Java-doc)
+	 * 
 	 * @see javax.servlet.http.HttpServlet#HttpServlet()
 	 */
 	public RecCabecalhos() {
 		super();
-	}   	 	
-	
-	/* (non-Java-doc)
-	 * @see javax.servlet.http.HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	}
+
+	/*
+	 * (non-Java-doc)
+	 * 
+	 * @see javax.servlet.http.HttpServlet#doPost(HttpServletRequest request,
+	 * HttpServletResponse response)
 	 */
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void service(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
 		response.setContentType("text/html;charset=ISO-8859-1");
-		String html = "<html>" +
-				"<head><title>Trabalhando com Servlet</title></head>" +
-				"</head>" +
-				"<body>";
-		Enumeration e = request.getHeaderNames( );
-		while(e.hasMoreElements( )){
-			String cabecalho = (String) e.nextElement( );
-			html+= "<strong>"+ cabecalho +": </strong>"+
-			request.getHeader(cabecalho); 
-			html+= "<br />";
+		String html = "<html>"
+				+ "<head><title>Trabalhando com Servlet</title></head>"
+				+ "</head>" + "<body>";
+		Enumeration<?> e = request.getHeaderNames();
+		while (e.hasMoreElements()) {
+			String cabecalho = (String) e.nextElement();
+			html += "<strong>" + cabecalho + ": </strong>"
+					+ request.getHeader(cabecalho);
+			html += "<br />";
 		}
-		
-		html+=	"</body>" +
-				"</html>";
-		PrintWriter out = response.getWriter( );
+
+		html += "</body>" + "</html>";
+		PrintWriter out = response.getWriter();
 		out.print(html);
 
-	}   	  	    
+	}
 }
--- ServletsComOutrosMets/src/meupacote/VarrendoTodosObjetos.java
+++ ServletsComOutrosMets/src/meupacote/VarrendoTodosObjetos.java
@@ -10,52 +10,64 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class for Servlet: VarrendoTodosObjetos
- *
+ * 
  */
- public class VarrendoTodosObjetos extends javax.servlet.http.HttpServlet {
-    /* (non-Java-doc)
+public class VarrendoTodosObjetos extends javax.servlet.http.HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4603789962581233444L;
+
+	/*
+	 * (non-Java-doc)
+	 * 
 	 * @see javax.servlet.http.HttpServlet#HttpServlet()
 	 */
 	public VarrendoTodosObjetos() {
 		super();
-	} 
-	
-	/* (non-Javadoc)
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see javax.servlet.Servlet#destroy()
 	 */
 	public void destroy() {
-		// TODO Auto-generated method stub
 		super.destroy();
-	}   	 	
-	
-	/* (non-Java-doc)
-	 * @see javax.servlet.http.HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Enumeration e = request.getParameterNames( ); 
-		String html = "<html><head>"+
-			"<title>Trabalhando com getParameterNames( )</title>"+
-			"</head>"+
-			"<body>" +
-			"<h2>Músicas escolhidas:</h2>";
-		while(e.hasMoreElements( )){ 	
-			String param = (String) e.nextElement( ); 
-			html+= "<strong>"+param+":</strong>"+ 
-				request.getParameter(param)+"<br />";		
-		}
-		html+="</body></html>";		
-	    response.setContentType("text/html");
-	    PrintWriter writer = response.getWriter( );
-	    writer.print(html);  
-	    writer.close( );	
+	}
 
-	}   	  	  
-	
-	/* (non-Javadoc)
+	/*
+	 * (non-Java-doc)
+	 * 
+	 * @see javax.servlet.http.HttpServlet#doPost(HttpServletRequest request,
+	 * HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		Enumeration<?> e = request.getParameterNames();
+		String html = "<html><head>"
+				+ "<title>Trabalhando com getParameterNames( )</title>"
+				+ "</head>" + "<body>" + "<h2>Músicas escolhidas:</h2>";
+		while (e.hasMoreElements()) {
+			String param = (String) e.nextElement();
+			html += "<strong>" + param + ":</strong>"
+					+ request.getParameter(param) + "<br />";
+		}
+		html += "</body></html>";
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+		writer.print(html);
+		writer.close();
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see javax.servlet.GenericServlet#init()
 	 */
 	public void init() throws ServletException {
-		// TODO Auto-generated method stub
 		super.init();
-	}   
+	}
 }
