4d52947758fc3f380cd4efde638a5e47aaf37931
Marcio Rosa <protonss@ig.com.br>
2010-08-19 08:22:22 -0300

    TESTANDO MODO GUI

--- EnvCaracteresEsp/src/meupacote/EnvCaractEspec.java
+++ EnvCaracteresEsp/src/meupacote/EnvCaractEspec.java
@@ -3,29 +3,24 @@ package meupacote;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URLEncoder;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
- public class EnvCaractEspec extends javax.servlet.http.HttpServlet {
-
-	public void destroy() {
-		super.destroy();
-	}   	
+ public class EnvCaractEspec extends javax.servlet.http.HttpServlet {	
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String queryString = 
-			URLEncoder.encode("Cidadão&João","ISO-8859-1");
+		
+		String queryString = URLEncoder.encode("Cidadão&João","ISO-8859-1");
+		
 		String html = "<html><head>"+
-		"<title>Enviando Caracteres especiais</title>"+
-		"</head>"+
-		"<body>" +
-		"<a href=\"RecCaractEspec?dados="+queryString+"\">" +
-				"Clique aqui para enviar" +
-		"</a>"+
-		"</body></html>";
+			"<title>Enviando Caracteres especiais</title>"+
+			"</head>"+
+			"<body>" +
+			"<a href=\"RecCaractEspec?dados="+queryString+"\">" +
+					"Clique aqui para enviar" +
+			"</a>"+
+			"</body></html>";
 	
 	    response.setContentType("text/html");
 	    PrintWriter writer = response.getWriter();
@@ -34,7 +29,4 @@ import javax.servlet.http.HttpServletResponse;
 		
 	}  	  	  	  
 
-	public void init() throws ServletException {
-		super.init();
-	}   
 }
--- EnvCaracteresEsp/src/meupacote/RecCaractEspec.java
+++ EnvCaracteresEsp/src/meupacote/RecCaractEspec.java
@@ -2,40 +2,29 @@ package meupacote;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class for Servlet: RecCaractEspec
- *
- */
- public class RecCaractEspec extends javax.servlet.http.HttpServlet {
-
-	public void destroy() {
-		super.destroy();
-	}   	
+public class RecCaractEspec extends javax.servlet.http.HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
 		String dados = request.getParameter("dados");
+
 		String html = "<html><head>"+
-		"<title>Recebendo caracteres especiais</title>"+
-		"</head>"+
-		"<body>"+
-		"Os dados recebidos são: <strong>"+dados+"</strong><br />"+
-		"A query string recebida é: <strong>"+ request.getQueryString()+"</strong>" +
-		"</body></html>";
+			"<title>Recebendo caracteres especiais</title>"+
+			"</head>"+
+			"<body>"+
+			"Os dados recebidos são: <strong>"+dados+"</strong><br />"+
+			"A query string recebida é: <strong>"+ request.getQueryString()+"</strong>" +
+			"</body></html>";
 	
-    response.setContentType("text/html");
-    PrintWriter writer = response.getWriter();
-    writer.print(html);  
-    writer.close();		
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+		writer.print(html);  
+		writer.close();		
 
 	}  	  	  	  
 
-
-	public void init() throws ServletException {
-		super.init();
-	}   
 }
